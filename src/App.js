1981a7001ac2413ff0e2c4bef25a397f702b60ce
MartinSimon08 <martin08poke@gmail.com>
2023-01-20 16:21:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
